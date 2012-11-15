711c9d95c3cc1afd5094624007f4bd3c8399934e
jcli <jcjc.li@gmail.com>
2012-11-15 12:18:24 -0800

    added some test files

---
+++ src_tests/FfmpegWrapperTest.h
@@ -0,0 +1,13 @@
+//
+//  FfmpegWrapperTest.h
+//  rtsp_player
+//
+//  Created by J.C. Li on 11/15/12.
+//  Copyright (c) 2012 J.C. Li. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FfmpegWrapperTest : SenTestCase
+
+@end
---
+++ src_tests/FfmpegWrapperTest.m
@@ -0,0 +1,13 @@
+//
+//  FfmpegWrapperTest.m
+//  rtsp_player
+//
+//  Created by J.C. Li on 11/15/12.
+//  Copyright (c) 2012 J.C. Li. All rights reserved.
+//
+
+#import "FfmpegWrapperTest.h"
+
+@implementation FfmpegWrapperTest
+
+@end
