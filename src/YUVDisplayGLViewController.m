108593780d0bf61ade9661b5f7853c2f24565941
jcli <jcjc.li@gmail.com>
2012-11-18 00:01:00 -0800

    made progress on OpenGL render

--- src/YUVDisplayGLViewController.m
+++ src/YUVDisplayGLViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "YUVDisplayGLViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
 
 typedef struct {
     float Position[3];
@@ -25,15 +28,57 @@ const GLubyte Indices[] = {
     2, 3, 0
 };
 
+#pragma mark - shaders
+
+#define STRINGIZE(x) #x
+#define STRINGIZE2(x) STRINGIZE(x)
+#define SHADER_STRING(text) @ STRINGIZE2(text)
+
+NSString *const vertexShaderString = SHADER_STRING
+(
+ attribute vec4 Position; // 1
+ attribute vec4 SourceColor; // 2
+ 
+ varying vec4 DestinationColor; // 3
+ 
+ void main(void) { // 4
+     DestinationColor = SourceColor; // 5
+     gl_Position = Position; // 6
+ }
+);
+
+NSString *const rgbFragmentShaderString = SHADER_STRING
+(
+ varying lowp vec4 DestinationColor; // 1
+ 
+ void main(void) { // 2
+     gl_FragColor = DestinationColor; // 3
+ }
+);
+
+
+#pragma mark - YUVDisplayGLViewController implementation
+
 @interface YUVDisplayGLViewController(){
     float _curRed;
     BOOL _increasing;
+    
+    GLuint _vertexBuffer;
+    GLuint _indexBuffer;
+    
+    GLuint _positionSlot;
+    GLuint _colorSlot;
 }
 @property (strong, nonatomic) EAGLContext *context;
 @end
 
 @implementation YUVDisplayGLViewController
 @synthesize context = _context;
+
+- (void) awakeFromNib
+{
+    [super awakeFromNib];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -42,6 +87,19 @@ const GLubyte Indices[] = {
         // Custom initialization
     }
     return self;
+}
+
+- (void)setupGL {
+    
+    [EAGLContext setCurrentContext:self.context];
+    
+    glGenBuffers(1, &_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 }
 
 - (void)viewDidLoad
@@ -56,6 +114,19 @@ const GLubyte Indices[] = {
     
     GLKView *view = (GLKView *)self.view;
     view.context = self.context;
+    
+    [self setupGL];
+    [self compileShaders];
+    
+}
+
+- (void)tearDownGL {
+    
+    [EAGLContext setCurrentContext:self.context];
+    
+    glDeleteBuffers(1, &_vertexBuffer);
+    glDeleteBuffers(1, &_indexBuffer);
+    
 }
 
 -(void)viewDidUnload
@@ -65,6 +136,8 @@ const GLubyte Indices[] = {
         [EAGLContext setCurrentContext:nil];
     }
     self.context = nil;
+    
+    [self tearDownGL];
 }
 - (void)didReceiveMemoryWarning
 {
@@ -72,12 +145,100 @@ const GLubyte Indices[] = {
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - compile and load shaders
+
+- (GLuint)compileShader:(NSString*)shaderString withType:(GLenum)shaderType
+{
+    GLuint shaderHandle = glCreateShader(shaderType);
+    if (shaderHandle == 0 || shaderHandle == GL_INVALID_ENUM) {
+        NSLog(@"Failed to create shader %d", shaderType);
+        exit(1);
+    }
+    // 3
+    const char * shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = [shaderString length];
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    // 4
+    glCompileShader(shaderHandle);
+    
+    // 5
+    GLint compileSuccess;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
+    if (compileSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    return shaderHandle;
+}
+
+- (void) compileShaders
+{
+    GLuint vertexShader = [self compileShader:vertexShaderString
+                                     withType:GL_VERTEX_SHADER];
+    GLuint fragmentShader = [self compileShader:rgbFragmentShaderString
+                                       withType:GL_FRAGMENT_SHADER];
+    
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragmentShader);
+    glLinkProgram(programHandle);
+    
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    glUseProgram(programHandle);
+    
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    _colorSlot = glGetAttribLocation(programHandle, "SourceColor");
+    glEnableVertexAttribArray(_positionSlot);
+    glEnableVertexAttribArray(_colorSlot);
+    
+}
+
+#pragma mark - render code
+
+- (void)render {
+    [EAGLContext setCurrentContext:self.context];
+//    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
+//    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // 1
+    glViewport(0, 0, 200, 200);
+    
+    // 2
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), 0);
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), (GLvoid*) (sizeof(float) * 3));
+    
+    // 3
+    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]),
+                   GL_UNSIGNED_BYTE, 0);
+    
+    [self.context presentRenderbuffer:GL_RENDERBUFFER];
+//    [self.context]
+}
+
+
 #pragma mark - GLKViewDelegate
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
     glClearColor(_curRed, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+    [self render];
 }
 
 #pragma mark - GLKViewControllerDelegate
